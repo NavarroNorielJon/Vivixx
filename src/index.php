50b33c7c2d2b9bc52d727cb1b42b1d54a2cf7a16
NavarroNorielJon <2161880@slu.edu.ph>
2018-06-06 16:50:23 +0800

    Update

--- src/index.php
+++ src/index.php
@@ -15,10 +15,10 @@
     </head>
     
     <body>
-        <div class="jumbotron">
+        <div class="jumbotron" style="padding-top: 6%;">
             <img src="img/logo2.png" style="width:40%; height:auto; margin-right:4%; margin-left:3%;"><br>
             <form action="../Utilities/login.php" method="post" class="col s12 ">
-                <div>
+                
                 <div class="form-group">
                     <label for="userOrEmail">Username or Email-Address</label>
                     <input class="form-control" type="text" name="user" id="userOrEmail" required="required" placeholder="Username or Email-Address">
@@ -28,7 +28,8 @@
                     <label for="pass">Password</label>
                     <input type="password" class="form-control" required="required" name="password" id="pass" placeholder="Password">
                 </div>
-                    
+                
+                <div style="text-align: center;">    
                 <button type="submit" class="btn btn-dark" style="" name="submit">Login</button>
                 <a a href="#!" class="modal-trigger" data-target="forgot" style="display: block; margin: 1rem;">Forgot password?</a>
                 </div>
