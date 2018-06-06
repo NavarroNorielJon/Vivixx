f796acd1f4487b47eb34093e1ec81e55f9c60b17
Marc Lawrence <gregoriorenz12@gmail.com>
2018-06-06 11:16:24 +0800

    update register

---
+++ src/JavaScript/ajax.js
@@ -0,0 +1,31 @@
+function helperText(type, str, elementId){
+	if(str.length == 0){
+		document.getElementById(elementId).innerHTML = "";
+		return;
+	} else {
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.onreadystatechange = function () {
+			if (this.readyState == 4 && this.status == 200){
+				document.getElementById(elementId).innerHTML = this.responseText;
+			}
+		};
+		xmlhttp.open("GET", "Utilities/validator.php?type=" + type + "&" + type + "=" + str, true);
+		xmlhttp.send();
+	}
+}
+function confirmPass(type, str, passElement, elementId){
+	var pass = document.getElementById(passElement).value;
+	if(str.length == 0){
+		document.getElementById(elementId).innerHTML = "";
+		return;
+	} else {
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.onreadystatechange = function () {
+			if (this.readyState == 4 && this.status == 200){
+				document.getElementById(elementId).innerHTML = this.responseText;
+			}
+		};
+		xmlhttp.open("GET", "Utilities/validator.php?type=" + type + "&" + type + "=" + srt + "&password=" + pass, true);
+		xmlhttp.send();
+	}
+}
--- src/Utilities/validator.php
+++ src/Utilities/validator.php
@@ -7,9 +7,14 @@ $type = $_REQUEST['type'];
 switch ($type){
 	case "email":
 		$email = $_REQUEST["email"];
-		if(!preg_match("/^[a-zA-Z0-9._%+-]+@[a-zAZ0-9.-]+\.[A-Za-z]{2,}$/"), $email){
+		$stmt = "SELECT * FROM user where email = '$email';";
+		$result = mysqli_query($connect, $stmt);
+		if(!preg_match("/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,}$/",$email)){
 			echo "<span class='invalid'>Invalid Email</span>";
+		} else if($result->num_rows > 0){
+			echo "<span class='invalid'>Email already exist</span>";
 		}
+		break;
 	case "username":
 		$username = $_REQUEST["username"];
 		$stmt = "SELECT * FROM user where username = '$username';";
@@ -17,5 +22,34 @@ switch ($type){
 		if($result->num_rows > 0){
 			echo "<span class='invalid'>Username already exist</span>";
 		}
+		break;
+	case "contact":
+		$contact_number = $_REQUEST["contact_number"];
+		if(!preg_match("/^09[0-9]{9}$/",$contact_number)){
+			echo "<span class='invalid'>Invalid Contact Number</span>";
+		}
+		break;
+	case "password":
+		$password = $_REQUEST["password"];
+		if(strlen($password) < 5 ){
+			echo "<span id='weak'>weak</span>";
+		} else if(strlen($password) > 5 || strlen($password) < 8){
+			echo "<span id='good'>good</span>";
+		} else if(strlen($password) > 8 || strlen($password) < 10){
+			echo "<span id='average'>average</span>";
+		} else if(strlen($password) > 10 || strlen($password) < 16){
+			echo "<span id='strong'>strong</span>";
+		}
+		break;
+	case "confirm_password":
+		$confirm = $_REQUEST["confirm_password"];
+		$password = $_REQUEST["password"];
+		if ($password !== $confirm){
+			echo "<span class='invalid'>Password doesn't match</span>";
+		} else {
+			echo "<span class='valid'>Password matches</span>";
+		}
+		break;
+		
 }
 ?>
