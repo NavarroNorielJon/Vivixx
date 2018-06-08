6e28a70988d9edc2e95a531ae7b17c28a3ff8b24
2161920 <2161920@slu.edu.ph>
2018-06-08 09:45:05 +0800

    update

---
+++ src/admin/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../style/bootstrap/bootstrap.min.css">
+  <script src="../script/jquery.min.js"></script>
+  <script src="../script/bootstrap/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-light" style="background-color:#005959">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="index.php" style="color:white">VIVIXX</a>
+    </div>
+    <ul class="nav navbar">
+      <li class="nav-item"><a class="nav-link" href="index.php" style="color:white">Home</a></li>
+      <li class="nav-item"><a class="nav-link" href="account_status.php" style="color:white">Accounts</a></li>
+      <li class="nav-item"><a class="nav-link" href="user_information.php" style="color:white">User</a></li>
+      <li class="nav-item"><a class="nav-link" href="logout.php" style="color:#fac213">Logout</a></li>
+    </ul>
+  </div>
+</nav>
+</body>
+</html>
