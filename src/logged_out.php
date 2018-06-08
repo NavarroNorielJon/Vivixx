4bcdc77edcb1ccb01a6dc1b25b7821ea0cd99b59
NavarroNorielJon <2161880@slu.edu.ph>
2018-06-08 09:40:58 +0800

    Cleaning

--- src/logged_out.php
+++ src/logged_out.php
@@ -1,50 +1,37 @@
 <!DOCTYPE html>
 <html>
-<head>
-</head>
-<body class="white">
-    <div class="jumbotron" id="login-form" style="background-color: white; padding-top: 6%;">
+    <body class="white">
+        <div class="jumbotron" id="login-form">
             <img src="img/Lion.png" style="width:40%; height:auto; margin-top: -10%; margin-right:4%; margin-left:3%;">
-
-            <form action="utilities/login.php" method="post" class="col s12 ">
-                <div class="form-group">
-                    <label for="userOrEmail">Username or Email-Address</label>
-                    <input class="form-control" type="text" onkeyup="confirmation('userOrEmail',this.value,'validUserOrEmail')" name="userOrEmail" id="userEmail" required="required" placeholder="Username or Email-Address">
-				    <div id="validUserOrEmail"></div>
-                </div>
-
-                <div class="form-group">
-                    <label for="pass">Password</label>
-
-                    <div class="input-group">
-                        <input type="password" placeholder="Password" name="password" id="lpass" class="form-control" onkeyup="confirmLogin('password',this.value,'userEmail','validPassword')" required="required" >
-
-                        <div class="input-group-append">
-                            <button  type="button" class="btn" onclick="showPass()">
-                            <i class="material-icons" >remove_red_eye</i>
-                            </button>
-                        </div>
+                <form action="utilities/login.php" method="post" class="col s12 ">
+                    <div class="form-group">
+                        <label for="userOrEmail">Username or Email-Address</label>
+                        <input class="form-control" type="text" onkeyup="confirmation('userOrEmail',this.value,'validUserOrEmail')" name="userOrEmail" id="userEmail" required="required" placeholder="Username or Email-Address">
+                        <div id="validUserOrEmail"></div>
                     </div>
-                    <div id="vPassword"></div>
-                </div>
 
-                <div style="text-align: center;">
-                    <button type="submit" class="btn" style="border: 2px solid #005959;" id="button1" name="submit">Login</button><br>
-                    <a a href="#!" data-toggle="modal" data-target="#forgot" style="display: block; margin: 1rem;">Forgot password?</a>
-                    <a href="signUp.php" style="display: block">Sign Up</a>
-                </div>
-            </form>
-        </div>
+                    <div class="form-group">
+                        <label for="pass">Password</label>
+                        <div class="input-group">
+                            <input type="password" placeholder="Password" name="password" id="lpass" class="form-control" onkeyup="confirmLogin('password',this.value,'userEmail','validPassword')" required="required" >
 
-        <div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle" type="button"  id="sample" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sample</button>
-        <div class="dropdown-menu" aria-labelledby="sample">
-            <a class="dropdown-item">Sample1</a>
-            <a class="dropdown-item">Sample2</a>
-            <a class="dropdown-item">Sample3</a>
-        </div>
-        </div>
+                            <div class="input-group-append">
+                                <button  type="button" class="btn" onclick="showPass()">
+                                <i class="material-icons">remove_red_eye</i>
+                                </button>
+                            </div>
+                        </div>
 
+                        <div id="vPassword"></div>
+                    </div>
+
+                    <div style="text-align: center;">
+                        <button type="submit" class="btn" style="border: 2px solid #005959;" id="button1" name="submit">Login</button><br>
+                        <a a href="#!" data-toggle="modal" data-target="#forgot" style="display: block; margin: 1rem;">Forgot password?</a>
+                        <a href="signUp.php" style="display: block">Sign Up</a>
+                    </div>
+                </form>
+        </div>
 
         <!-- Modal for forgot password -->
         <div class="modal fade" id="forgot" tabindex="-1" role="dialog">
@@ -75,6 +62,5 @@
                 </div>
             </div>
         </div>
-        <script type="text/javascript" src="script/ajax.js"></script>
-</body>
+    </body>
 </html>
