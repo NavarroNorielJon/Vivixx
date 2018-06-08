5e9af874a04831323cd3688ae677becfbf434011
NavarroNorielJon <2161880@slu.edu.ph>
2018-06-08 09:45:18 +0800

    Clean

--- src/logged_in.php
+++ src/logged_in.php
@@ -1,11 +1,9 @@
 <!DOCTYPE html>
 <html>
-
-<body class="white">
-    <div class='center-align'>
-        <h1>Welcome to Vivixx Academy <a href="utilities/logout.php"><?php echo "$user_first!"?></a></h1>
-    </div>
-    <script type="text/javascript" src="JavaScript/js/materialize.min.js"></script>
-</body>
-    
+    <body class="white">
+        <div class='center-align'>
+            <h1>Welcome to Vivixx Academy <a href="utilities/logout.php"><?php echo "$user_first!"?></a></h1>
+        </div>
+        <script type="text/javascript" src="JavaScript/js/materialize.min.js"></script>
+    </body>
 </html>
