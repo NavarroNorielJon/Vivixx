50f51c9438baa91a50248bffd03d11aaddeb4509
2161920 <2161920@slu.edu.ph>
2018-06-11 15:57:29 +0800

    added audio

--- src/admin/index.php
+++ src/admin/index.php
@@ -5,6 +5,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<audio autoplay>
+  <source src="../audio/welcome.mp3" type="audio/mpeg">
+</audio>
+
   <title>Bootstrap Example</title>
   <link rel="stylesheet" href="../style/bootstrap/bootstrap.min.css">
   <link type="text/css" rel="stylesheet" href="../style/bootstrap/bootstrap.min.css" media="screen, projection">
