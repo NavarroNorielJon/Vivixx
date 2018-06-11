e62e6145ae6fddc6e6276f2caf3310327a23ede9
Marc Lawrence <gregoriorenz12@gmail.com>
2018-06-11 10:13:10 +0800

    fixed add function

--- src/signUp.php
+++ src/signUp.php
@@ -317,17 +317,20 @@
                    <div class="row">
                        <div class="form-group col-6">
                            <label for="child_name">Name</label>
-                           <input type="text" placeholder="First name Middle initial. Last name" name="child_name" id="child_name" class="form-control" required="required">
+                           <input type="text" placeholder="First name M.I. Last name" name="child_name[]" id="child_name" class="form-control" required="required">
                        </div>
 
                        <div class="form-group col-6">
                            <label for="child_birth">Date of Birth</label>
-                           <input type="date" name="child_birth" id="child_birth" class="form-control" required="required">
+                           <div class="input-group">
+                               <input type="date" name="child_birth[]" id="child_birth" class="form-control" required="required">
+                               <div class="input-group-append">
+                                   <button class="btn btn-success" type="button" onclick="add()">&plus;</button>
+                               </div>
+                           </div>
                        </div>
-
                    </div>
-                   <div style="text-align: right; margin-bottom:2%;">
-                       <button type="button">Add</button>
+                   <div id="child">
                    </div>
 
                    <div style="text-align: right">
