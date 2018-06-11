12d1d5bdf7e92cb63666744aa72f9006d996746f
Marc Lawrence <gregoriorenz12@gmail.com>
2018-06-11 16:59:20 +0800

    add

--- src/utilities/login.php
+++ src/utilities/login.php
@@ -5,7 +5,7 @@
     if(isset($_POST["userOrEmail"]) && isset($_POST["password"])){
         $user = mysqli_real_escape_string($connect, $_POST["userOrEmail"]);
         $password = mysqli_real_escape_string($connect, $_POST["password"]);
-        $stmt = "SELECT username, password FROM user WHERE username = '$user' or email = '$user' ";
+        $stmt = "SELECT username, password, type FROM user WHERE username = '$user' or email = '$user' ";
         $results = mysqli_query($connect, $stmt);
         $row = mysqli_fetch_array($results, MYSQLI_ASSOC);
         $count = mysqli_num_rows($results);
@@ -13,7 +13,7 @@
         if ($count == 1) {
             if (password_verify($password, $passwordVerify)) {
                 $_SESSION['user'] = $user;
-                $test = "SELECT * FROM user_info NATURAL JOIN user WHERE username='$user' and birth_place is null";
+                $test = "SELECT * FROM user_info NATURAL JOIN user WHERE birthdate ='null'";
                 $result = mysqli_query($connect,$test);
                 if($result->num_rows > 0){
                     header('location:/information');
