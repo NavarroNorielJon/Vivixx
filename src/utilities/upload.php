73a3a1923bf9860c7e8e7f5a281f54fd5b058390
NavarroNorielJon <2161880@slu.edu.ph>
2018-06-11 15:11:12 +0800

    Update

--- src/utilities/upload.php
+++ src/utilities/upload.php
@@ -1,19 +1,27 @@
 <?php
 	include 'session.php';
-	$directory = "../img/profile-images";
-	$file = $directory . basename($_FILES["prof-image"]["name"]);
+	$connect = Connect();
+	$directory = "../img/profile-images/";
+	$file_name = $_FILES["image"]["name"];
+	$file_tmp = $_FILES["image"]["tmp_name"];
 	$uploadSuccess = 1;
-	$fileType = strtolower(pathinfo($file,PATHINFO_EXTENSION));
+	$fileType = strtolower(pathinfo($file_tmp,PATHINFO_EXTENSION));
+	$data = base64_decode(file_get_contents($file_tmp));
+
 
 	if(isset($_POST["submit"])) {
-		$verify = getimagesize($_FILES["prof-image"]["tmp_name"]);
+		$verify = getimagesize($_FILES["image"]["tmp_name"]);
+		$stmt = "INSERT INTO user_info (`profile_image`) VALUES ('$data')";
+		$result = mysqli_query($connect, $stmt);
 
 		if($verify != false) {
-			echo "<script>alert('File Successfully Uploaded');</script>";
-			$uploadSuccess = 1;
+				echo "<script>alert('Lahat kayo matalino!');</script>";
+			// move_uploaded_file($_FILES["prof-image"]['tmp_name'],$file);
 		} else {
 			echo "<script>alert('File upload failed');</script>";
 			$uploadSuccess = 0;
 		}
 	}
+
+
 ?>
