dcf156454eac427b691077c1131f58ccc83b3046
Marc Lawrence <gregoriorenz12@gmail.com>
2018-06-12 18:37:43 +0800

    up

--- src/admin/update_status.php
+++ src/admin/update_status.php
@@ -4,10 +4,10 @@
 	$connect = Connect();
 	$username = $_POST["username"];
 		if(isset($_POST["enable"])){
-    		$update = "UPDATE mis.user SET status='enabled' WHERE username='$username'";
+    		$update = "UPDATE user SET status='enabled' WHERE username='$username'";
 
     	}else{
-    		$update ="UPDATE mis.user SET status='disabled' WHERE username='$username'";
+    		$update ="UPDATE user SET status='disabled' WHERE username='$username'";
     	}
 	
 	$result = $connect->query($update);
