f0d6f055627b62da5d834c5225049d990ec68f72
NavarroNorielJon <2161880@slu.edu.ph>
2018-06-12 15:00:04 +0800

    Cleaned the admin side

--- src/admin/data_information.php
+++ src/admin/data_information.php
@@ -1,58 +1,60 @@
 <!DOCTYPE html>
 <html>
 <head>
-	 <title>Vivixx</title>
-		<meta charset="utf-8">
-        <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
-        <link type="text/css" rel="stylesheet" href="../style/bootstrap/bootstrap.min.css" media="screen, projection">
-        <link type="text/css" rel="stylesheet" href="../style/style.css" media="screen, projection">
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Vivixx</title>
+	<meta charset="utf-8">
+	<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+	<link type="text/css" rel="stylesheet" href="../style/bootstrap/bootstrap.min.css" media="screen, projection">
+    <link type="text/css" rel="stylesheet" href="../style/style.css" media="screen, projection">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
+	
 <body>
-<table class="table" id="table">
-  <thead>
-    <tr>
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>Gender</th>
-        <th>Address</th>
-        <th>Contact Number</th>
-        <th>Email</th>
-        <th>Show more data</th>
-    </tr>
-  </thead>
-<?php
-include '../Utilities/db.php';
-$connect = Connect();
+	<table class="table" id="table">
+  		<thead>
+    		<tr>
+        		<th>First Name</th>
+        		<th>Last Name</th>
+        		<th>Gender</th>
+        		<th>Address</th>
+        		<th>Contact Number</th>
+        		<th>Email</th>
+        		<th>Show more data</th>
+    		</tr>
+  		</thead>
+		
+		<?php
+			include '../Utilities/db.php';
+			$connect = Connect();
+			$sql = "select * from user_info natural join user;";
+			$result = $connect->query($sql);
+			
+			if($result-> num_rows > 0){
+    			while($row = $result->fetch_assoc()){
+        			if($row["gender"] === "m"){
+            			$gender = "Male";
+        			}else{
+            			$gender = "Female";
+        	}
 
-$sql = "select * from user_info natural join user;";
-$result = $connect->query($sql);
-
-if($result-> num_rows > 0){
-    while($row = $result->fetch_assoc()){
-        if($row["gender"] === "m"){
-            $gender = "Male";
-        }else{
-            $gender = "Female";
-        }
-
-        $show = "
-        <input name='show' value='show' style='display: none;'>
-        <a href='#!' data-toggle='modal' data-target='#1st'><button class='btn btn-warning' type='submit' value='show'>Show more</a>";
-        //print data in table
-        echo "
-        <form action='show_all' method='POST'>
-        <tr>
-        <td>" . ucwords($row['first_name']) . "</td>
-        <td>" . ucwords($row['last_name']) . "</td>
-        <td>" . $gender . "</td>
-        <td>" . $row['residential_address'] . "</td>
-        <td>" . $row['contact_number'] . "</td>
-        <td>" . $row['email'] . "</td>
-        <td>" . $show ."</td>
-        </form>
-        </tr>";
+        	$show = "
+        			<input name='show' value='show' style='display: none;'>
+        			<a href='#!' data-toggle='modal' data-target='#1st'><button class='btn btn-warning' type='submit' value='show'>Show more</a>";
+        	
+			//print data in table
+        	echo "
+        		<form action='show_all' method='POST'>
+					<tr>
+						<td>" . ucwords($row['first_name']) . "</td>
+						<td>" . ucwords($row['last_name']) . "</td>
+						<td>" . $gender . "</td>
+						<td>" . $row['residential_address'] . "</td>
+						<td>" . $row['contact_number'] . "</td>
+						<td>" . $row['email'] . "</td>
+						<td>" . $show ."</td>
+					</tr>
+        		</form>";
     }
 
 }else{
--- src/admin/user_information.php
+++ src/admin/user_information.php
@@ -4,38 +4,43 @@
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Table</title>
-        <script type="text/javascript" src="../script/jquery-3.2.1.min.js"></script>
-        <script type="text/javascript" src="../script/datatables.min.js"></script>
-        <link rel="stylesheet" href="../style/bootstrap/bootstrap.min.css">
-        <link rel="stylesheet" href="../style/datatables.css">
-        <link type="text/css" rel="stylesheet" href="../style/style.css" media="screen, projection">
-        <script type="text/javascript" src="../script/popper.min.js"></script>
-        <script type="text/javascript" src="../script/bootstrap/bootstrap.min.js"></script>
-    </head>
-    <body>
-          <h1>User Information</h1>
-          <div id="content">
-          <?php
-          include 'data_information.php';
-           ?>
-         </div>
-         <!--script for calling data table library-->
-         <script>
-         $(document).ready(function(){
-         $('#table').dataTable( {
-          "columnDefs": [
-            { "orderable": false, "targets": 5 }
+<head>
+	<title>Table</title>
+    <script type="text/javascript" src="../script/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="../script/datatables.min.js"></script>
+    <link rel="stylesheet" href="../style/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="../style/datatables.css">
+    <link type="text/css" rel="stylesheet" href="../style/style.css" media="screen, projection">
+</head>
 
-          ],
-          "responsive": true,
-          "order": [
-            [4, "desc"]
-          ]
-        } );
-         $('#table').DataTable();
-         });
-        </script>
+<body>
+	<div id="content">
+		<h1>User Information</h1>
+        <?php
+        	include 'data_information.php';
+        ?>
+	</div>
+	
+	<script type="text/javascript" src="script/ajax.js"></script>
+	<script type="text/javascript" src="script/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="script/popper.min.js"></script>
+	<script type="text/javascript" src="script/bootstrap/bootstrap.min.js"></script>
+	<script type="text/javascript" src="script/sweetalert.min.js"></script>
+	<!--script for calling data table library-->
+	<script>
+    	$(document).ready(function(){
+			$('#table').dataTable( {
+				"columnDefs": [
+					{ "orderable": false, "targets": 5 }
+				],
+				"responsive": true,
+				"order": [
+					[4, "desc"]
+				]
+			} );
+			$('#table').DataTable();
+		});
+	</script>
+	
     </body>
 </html>
