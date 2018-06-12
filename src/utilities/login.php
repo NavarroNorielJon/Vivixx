a3a522c139e791352b3073ef53d5a9000f3e554b
Marc Lawrence <gregoriorenz12@gmail.com>
2018-06-12 16:01:14 +0800

    Merge branch 'master' of https://github.com/NavarroNorielJon/Vivixx

--- src/utilities/login.php
+++ src/utilities/login.php
@@ -18,7 +18,6 @@
                 $_SESSION['user'] = $user;
                 $result = mysqli_query($connect,$test);
                 if($result->num_rows > 0){
-<<<<<<< HEAD
                     header('location:update_information');
                     echo "<script>
                             alert('Invalid username or password');
@@ -26,12 +25,6 @@
                     // if(isset($_SESSION['']) && ){
                     //     header('location:update_information');
                     // }
-=======
-                    header('location:/update_information');
-                    if(isset($_SESSION['birth_date'])){
-                        header('location:/update_information');
-                    }
->>>>>>> f17b507a4e86d4c780b8f3329aab7c96b3284b32
                 }else{
                     header('location:/home');
                 }
