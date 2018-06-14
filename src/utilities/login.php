ded9238d3da8354b56fce13037eaaaef795966b9
Marc Lawrence <gregoriorenz12@gmail.com>
2018-06-14 10:27:01 +0800

    update

--- src/utilities/login.php
+++ src/utilities/login.php
@@ -2,57 +2,56 @@
     include 'db.php';
     session_start();
     $connect = Connect();
-    if(isset($_POST["userOrEmail"]) && isset($_POST["login_password"])){
+    if (isset($_POST["userOrEmail"]) && isset($_POST["login_password"])) {
         $user = mysqli_real_escape_string($connect, $_POST["userOrEmail"]);
         $password = mysqli_real_escape_string($connect, $_POST["login_password"]);
-        $stmt = "SELECT username, password, type, status FROM user WHERE username = '$user' or email = '$user' ";
-        $results = mysqli_query($connect, $stmt);
-        $row = mysqli_fetch_array($results, MYSQLI_ASSOC);
-        $count = mysqli_num_rows($results);
-        $passwordVerify = $row['password'];
-        $type = $row["type"];
-        $status = $row["status"];
-        if ($count == 1) {
-            if (password_verify($password, $passwordVerify) && $status === "enabled") {
-                if($_SESSION['user'] = $user && $type === "user" ){
-                $test = "SELECT * FROM user_info NATURAL JOIN user WHERE (email='$user' or username='$user') and birth_place is null";
-                $_SESSION['user'] = $user;
-                $result = mysqli_query($connect,$test);
-                if($result->num_rows > 0){
-                    header('location:update_information');
+        $sql = "SELECT username, email FROM user WHERE username = '$user' or email ='$user';";
+        $result = mysqli_query($connect,$sql);
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        if ($user === $row['username']) {
+            $stmt = "SELECT username, password, type, status FROM user WHERE username = '$user' or email = '$user';";
+            $results = mysqli_query($connect, $stmt);
+            $row = mysqli_fetch_array($results, MYSQLI_ASSOC);
+            $count = mysqli_num_rows($results);
+            $passwordVerify = $row['password'];
+            $type = $row["type"];
+            $status = $row["status"];
+            if ($count == 1) {
+                if (password_verify($password, $passwordVerify) && $status === "enabled") {
+                    if ($_SESSION['user'] = $user && $type === "user") {
+                        $test = "SELECT * FROM user_info NATURAL JOIN user WHERE (email='$user' or username='$user') and birth_place is null";
+                        $_SESSION['user'] = $user;
+                        $result = mysqli_query($connect,$test);
+                        if ($result->num_rows > 0 ) {
+                            header('location:update_information');
+                        } else {
+                            header('location:/home');
+                        }
+                    } elseif ($type === "admin") {
+                        header('location:../admin/index');
+                    }
+                }elseif (!password_verify($password, $passwordVerify) && $status === "enabled") {
                     echo "<script>
-                            alert('Invalid username or password');
+                            alert('Invalid password');
+                            window.location = '/';
                           </script>";
-                    // if(isset($_SESSION['']) && ){
-                    //     header('location:update_information');
-                    // }
-
-                }else{
-                    header('location:/home');
+                }elseif (password_verify($password, $passwordVerify) && $status === "disabled") {
+                    echo "<script>
+                            alert('Your account is disabled');
+                            window.location = '/';
+                          </script>";
                 }
-            } elseif($type === "admin"){
-                header('location:../admin/index');
-            }
-            }elseif(!password_verify($password, $passwordVerify) && $status === "enabled") {
+            } else{
                 echo "<script>
-                        alert('Invalid username or password or disabled');
-                        window.location = '/';
-                      </script>";
-
-
-
-			}else{
-                echo "<script>
-                        alert('Account is disabled');
+                        alert('User does not exist');
                         window.location = '/';
                       </script>";
             }
-        }else {
-			echo "<script>
-                    alert('User does not exist');
-					window.location = '/';
-				  </script>";
-		}
+        } else {
+            echo "<script>
+                    alert('Invalid username or password.');
+                    window.location = '/';
+                  </script>";
+        }
     }
-
 ?>
