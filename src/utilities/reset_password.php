d7262855e03a391d4d937bd108fe0c63f8c4eeaf
2161920 <2161920@slu.edu.ph>
2018-06-15 15:50:51 +0800

    update

--- src/utilities/reset_password.php
+++ src/utilities/reset_password.php
@@ -10,10 +10,14 @@
 </head>
 <body>
     <form action="validate_reset_password.php" method="POST">
-        <input type="text" style="display:none" name="account"value="<?php echo $_GET['account']?>">
-        <input type="password" name="new_pass" placeholder="Enter New password">
-        <input type="password" name="confirm_pass" placeholder="Confirm password">
-        <input type="submit" name="submit" value="submit">
+        <div class="form-group" style="text-align: center;">
+            <input type="text" style="display:none" name="account"value="<?php echo $_GET['account']?>">
+            <input type="password" name="new_pass" placeholder="Enter New password">
+            <br>
+            <input type="password" name="confirm_pass" placeholder="Confirm password">
+            <br>
+            <input type="submit" name="submit" value="submit">
+        </div>
     </form>
 </body>
 </html>
