837aa2af91ad3d1850ee0d24a9ac1d4969742c23
2161920 <2161920@slu.edu.ph>
2018-06-15 15:20:32 +0800

    update

---
+++ src/utilities/reset_password.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+</head>
+<body>
+    <form action="validate_reset_password.php" method="POST">
+        <input type="text" name="new_pass" placeholder="Enter New password">
+        <input type="text" name="confirm_pass" placeholder="Confirm password">
+        <input type="submit" name="submit" value="submit">
+    </form>
+</body>
+</html>
