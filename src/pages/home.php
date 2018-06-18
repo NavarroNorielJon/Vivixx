1ebd6d6188ee8888133409859be78f934f443880
Marc Lawrence <gregoriorenz12@gmail.com>
2018-06-18 14:35:34 +0800

    up

--- src/pages/home.php
+++ src/pages/home.php
@@ -25,20 +25,27 @@
 
 			<!-- Sidebar Links -->
         	<ul class="list-unstyled components">
-
+				<li>
+					<a href="profile.php" class="sidebar-item">
+					<i class="material-icons">person</i> <?php echo "$first_name"?></a>
+					<a href="profile.php" class="icon"><i class="material-icons">person</i></a>
+				</li>
+				
             	<li class="active">
 					<a href="profile.php" class="sidebar-item">
 					<i class="material-icons">home</i>Home</a>
 					<a class="icon" href="home.php"><i class="material-icons">home</i></a>
 				</li>
-
+				
 				<li>
-					<a href="#" class="icon">SR</a>
-					<a href="leave_request_form.php" class="icon">LR</a>
 					<a href="#requests" data-toggle="collapse" class="sidebar-item" aria-expanded="false"><i class="material-icons">work</i> Requests</a>
+					<a href="#requests" data-toggle="collapse" class="icon" aria-expanded="false"><i class="material-icons">work</i></a>
 					<ul class="collapse list-unstyled" id="requests">
-						<li class="active"><a href="#">Salary Request</a></li>
-						<li class="active"><a href="leave_request_form">Leave Request</a></li>
+						<li class="active"><a href="#" class="sidebar-item">Salary Request</a></li>
+						<li class="active"><a href="leave_request_form" class="sidebar-item">Leave Request</a></li>
+						
+						<li class="active"><a href="#requests" class="icon">SR</a></li>
+						<li class="active"><a href="leave_request_form.php" class="icon">LR</a></li>
 					</ul>
 				</li>
             	<li>
@@ -51,7 +58,7 @@
 					<a href="../utilities/logout.php"  class="sidebar-item" id="logout">
 					<i class="material-icons">power_settings_new</i> Logout
 					</a>
-					<a class="icon" href="../utilities/logout.php"><i class="material-icons">power_settings_new</i></a>
+					<a class="icon" href="../utilities/logout.php"><i class="material-icons">power_settings_new</i></a>						
 				</li>
         	</ul>
 		</nav>
