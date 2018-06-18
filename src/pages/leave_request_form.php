307dd0d8d464b9231b31a6392f55935b2fd2bb33
Marc Lawrence <gregoriorenz12@gmail.com>
2018-06-18 15:56:39 +0800

    up

--- src/pages/leave_request_form.php
+++ src/pages/leave_request_form.php
@@ -13,61 +13,82 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 
-<body>
-
+<body style="background-color: #e6e6e6;">
 	<div class="wrapper">
 		<nav id="sidebar">
 			<div class="sidebar-header">
-				<img src="../img/Lion.png">
+				<a class="sidebar-logo" href="home"><img src="../img/Lion.png"></a>
 			</div>
 
 			<!-- Sidebar Links -->
-        	<ul class="list-unstyled components" data-spy="affix">
-
-				<li><a href="profile"><i class="material-icons">person</i> <?php echo "$first_name"?></a></li>
-				<li><a href="home"><i class="material-icons">home</i> Home</a></li>
-				<li class="active">
-					<a href="#requests" data-toggle="collapse" aria-expanded="false"> <i class="material-icons">work</i> Requests</a>
+        	<ul class="list-unstyled components">
+				<li>
+					<a href="profile.php" class="sidebar-item">
+					<i class="material-icons">person</i> <?php echo "$first_name"?></a>
+					<a href="profile.php" class="icon"><i class="material-icons">person</i></a>
+				</li>
+				
+            	<li>
+					<a href="profile.php" class="sidebar-item">
+					<i class="material-icons">home</i>Home</a>
+					<a class="icon" href="home.php"><i class="material-icons">home</i></a>
+				</li>
+				
+				<li  class="active">
+					<a href="#requests" data-toggle="collapse" class="sidebar-item" aria-expanded="false"><i class="material-icons">work</i> Requests</a>
+					<a href="#requests" data-toggle="collapse" class="icon" aria-expanded="false"><i class="material-icons">work</i></a>
 					<ul class="collapse list-unstyled" id="requests">
-						<li class="active"><a href="#">Salary Request</a></li>
-						<li class="active"><a href="leave_request_form">Leave Request</a></li>
+						<li class="active"><a href="#" class="sidebar-item">Salary Request</a></li>
+						<li class="active"><a href="leave_request_form" class="sidebar-item">Leave Request</a></li>
+						
+						<li class="active"><a href="#requests" class="icon">SR</a></li>
+						<li class="active"><a href="leave_request_form.php" class="icon">LR</a></li>
 					</ul>
 				</li>
-            	<li><a href="#"> <i class="material-icons">info_outline</i> About</a></li>
-            	<li><a href="../utilities/logout.php" id="logout">
-						<i class="material-icons">power_settings_new</i> Logout</a></li>
+            	<li>
+					<a href="#" class="sidebar-item">
+						<i class="material-icons">info_outline</i> About
+					</a>
+					<a class="icon"><i class="material-icons">info_outline</i></a>
+				</li><hr>
+            	<li>
+					<a href="../utilities/logout.php"  class="sidebar-item" id="logout">
+					<i class="material-icons">power_settings_new</i> Logout
+					</a>
+					<a class="icon" href="../utilities/logout.php"><i class="material-icons">power_settings_new</i></a>						
+				</li>
         	</ul>
 		</nav>
 
-		<div id="content">
-			<form id="leave_form" action="../utilities/leave_request" method="POST">
+		<div id="leave">
+			<form id="leave_form" action="" method="POST">
 				<h1 class="text-center">LEAVE APPLICATION FORM</h1><hr>
 					<div class="row">
 						<div class="form-group col">
 							<label for="employee_name">Employee</label>
-							<input type="text" class="form-control-plaintext" id="employee_name" placeholder="<?php echo $full_name?>" name="employee_name" value="<?php echo $full_name?>" disabled>
+							<input type="text" class="form-control-plaintext" id="employee_name" placeholder="<?php echo $full_name?>" name="employeeName" value="<?php echo $full_name?>" disabled>
 						</div>
 
 						<div class="form-group col">
 							<label for="department">Department</label>
-							<input type="text" class="form-control" id="department" placeholder="Department" name="department">
+							<input type="text" class="form-control" id="department" placeholder="Department" name="dept">
 						</div>
 					</div>
 
 				<div class="row">
 					<div class="form-group col">
 						<label for="position">Position</label>
-						<input type="text" class="form-control" id="position" placeholder="Position" name="position">
+						<input type="text" class="form-control" id="position" placeholder="Position" name="employeePosition">
 					</div>
 
 					<div class="form-group col">
 						<label for="date_hired">Date Hired</label>
-						<input type="date" class="form-control" id="date_hired" placeholder="Date Hired" name="date_hired">
+						<input type="date" class="form-control" id="date_hired" placeholder="Date Hired" name="dateHired">
 					</div>
 
 					<div class="form-group col">
 						<label for="date_filed">Date Filed</label>
-						<input type="date" class="form-control" id="date_filed" placeholder="date_filed" name="date_filed">
+						<input type="date" class="form-control" id="date_filed" placeholder="date_filed" name="dateFilled">
 					</div>
 				</div><hr>
 
@@ -91,12 +112,12 @@
 					<div class="row">
 						<div class="form-group col">
 							<label for="address_leave">Contact Address during leave</label>
-							<input type="text" class="form-control" id="address_leave" name="contact_address">
+							<input type="text" class="form-control" id="address_leave">
 						</div>
 
 						<div class="form-group col">
 							<label for="number_leave">Contact Number during leave</label>
-							<input type="text" class="form-control" id="number_leave" name="contact_number">
+							<input type="text" class="form-control" id="number_leave">
 						</div>
 					</div>
 				</div><hr>
@@ -107,17 +128,17 @@
 					<div class="row">
 						<div class="form-group col">
 							<label for="start_date">From</label>
-							<input type="date" class="form-control" name="from">
+							<input type="date" class="form-control" >
 						</div>
 
 						<div class="form-group col">
 							<label for="end_date">To</label>
-							<input type="date" class="form-control" id="end_date" name="to">
+							<input type="date" class="form-control" id="end_date">
 						</div>
 					</div>
 
 					<div style="text-align:right">
-						<button class="btn btn-success">Submit</button>
+						<button class="btn btn-primary">Submit</button>
 					</div>
 
 				</div>
