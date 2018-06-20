94e1c7d652d11cfd98830cde55ea6feff9ca1436
NavarroNorielJon <2161880@slu.edu.ph>
2018-06-20 13:09:17 +0800

    Update

--- src/admin/navbar.php
+++ src/admin/navbar.php
@@ -17,31 +17,39 @@
     </head>
 	
 <body>
-	<nav class="navbar navbar-expand-lg navbar-dark" id="navigation-bar">
-<!--		<a href="#!"><img src="../img/Lion.png" id="nav-logo"></a>-->
-			<a href="#!" class="navbar-brand">Vivixx</a>
-		<div class="ml-auto">
-			<ul class="navbar-nav">
-				<li class="nav-item active">
-					<a class="nav-link" href="#">Home</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">Accounts</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">Users</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">Leaver Request</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">Summary of Pay</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link logout" href="#">Logout</a>
-				</li>
-			</ul>
-		</div>
-	</nav>
+	
+	<div class="wrapper">
+		<nav class="navbar fixed-top navbar-expand-lg navbar-dark" id="navigation-bar">
+			<!--<a href="#!"><img src="../img/Lion.png" id="nav-logo"></a>-->
+			<a href="index" class="navbar-brand">Vivixx</a>
+			
+			<div class="ml-auto">
+				<ul class="navbar-nav">
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Accounts</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Users</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Leaver Request</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Summary of Pay</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link logout" href="#">Logout</a>
+					</li>
+				</ul>
+			</div>
+			
+			<div id="index-content">
+			
+			</div>
+		</nav>
+	</div>	
 </body>
 </html>
