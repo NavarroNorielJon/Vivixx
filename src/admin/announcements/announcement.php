1a44880e5c1c738c0681a4aba932cf1236ad507a
NavarroNorielJon <2161880@slu.edu.ph>
2018-06-21 15:52:22 +0800

    Update

--- src/admin/announcements/announcement.php
+++ src/admin/announcements/announcement.php
@@ -1,11 +1,65 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Vivixx</title>
-    <link type="text/css" rel="stylesheet" href="../../style/style2.css">
+	<title>Vivixx Ph</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" href="../../style/bootstrap/bootstrap.min.css">
+	<link type="text/css" rel="stylesheet" href="../style/jquery-ui.min.css">
+
+	<link type="text/css" rel="stylesheet" href="../style/style.css" media="screen, projection">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="../../style/datatables.css">
+	
+    <!--scripts-->
+	<script src="../../script/jquery.min.js"></script>
+	<script type="text/javascript" src="../../script/bootstrap/bootstrap.min.js"></script>
+	<script src="../../script/jquery.form.min.js"></script>
+	<script type="text/javascript" src="../../script/jquery-ui.min.js"></script>
+	<script type="text/javascript" src="../../script/popper.min.js"></script>
+	<script type="text/javascript" src="../../script/ajax.js"></script>  
+	
 </head>
 <body>
-    <form action="announcements/submit_announcement.php" class="text-center" id="container-announcement" method="POST" enctype="multipart/form-data">
+	
+	<div id="wrapper">
+		<nav class="navbar fixed-top navbar-expand-lg navbar-dark" id="navigation-bar">
+			<!--<a href="#!"><img src="../img/Lion.png" id="nav-logo"></a>-->
+			<a href="index" class="navbar-brand" style="margin-right:40vw;">Vivixx</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-content" aria-controls="#navbar-content" aria-expanded="false" aria-label="Toggle navigation">
+    			<span class="navbar-toggler-icon"></span>
+			</button>
+			
+			<div class="collapse navbar-collapse" id="navbar-content">
+				<ul class="navbar-nav">
+					<li class="nav-item active">
+						<a class="nav-link" href="index">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="accounts/accounts_status">Accounts</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="user_information/user_information">Users</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="leave_request/leave_requests">Leaver Request</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Summary of Pay</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="announcements/view_announcement.php">Announcement</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link logout" href="logout.php">Logout</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+		
+		<div class="announcement-content container-fluid">
+			<form action="announcements/submit_announcement.php" class="text-center" id="container-announcement" method="POST" enctype="multipart/form-data">
 		<div class="row form-goup">
 			<div class="col">
 				<input name="subject" type="text" class="form-control" placeholder="Subject" required>
@@ -56,7 +110,11 @@
             </div>
         </div>
     </div>	
+		</div>
+	</div>
 </body>
+	
+	
 <script>
 $("#date").datepicker({
     showButtonPanel: true
