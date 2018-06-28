a3c0d3610e12625870d8ca4b86d99bf6c426b54a
NavarroNorielJon <2161880@slu.edu.ph>
2018-06-28 10:56:11 +0800

    Admin Update

--- src/admin/announcements/announcement.php
+++ src/admin/announcements/announcement.php
@@ -27,17 +27,13 @@
 <body style="background-color:white !important;">
 	<div id="wrapper">
 		<nav class="navbar fixed-top navbar-expand-lg navbar-dark" id="navigation-bar">
-			<!--<a href="#!"><img src="../img/Lion.png" id="nav-logo"></a>-->
-			<a href="../index" class="navbar-brand" style="margin-right:40vw;">Vivixx</a>
+			<a href="../index" class="navbar-brand" style="margin-right:53vw;">Vivixx</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-content" aria-controls="#navbar-content" aria-expanded="false" aria-label="Toggle navigation">
     			<span class="navbar-toggler-icon"></span>
 			</button>
 			
 			<div class="collapse navbar-collapse" id="navbar-content">
 				<ul class="navbar-nav">
-					<li class="nav-item">
-						<a class="nav-link" href="../index.php">Home</a>
-					</li>
 					<li class="nav-item ">
 						<a class="nav-link" href="../accounts/accounts_status.php">Accounts</a>
 					</li>
@@ -54,7 +50,7 @@
 						<a class="nav-link active" href="announcement.php">Announcement</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link logout" href="../logout.php">Logout</a>
+						<a class="nav-link logout" href="../utilities/logout.php">Logout</a>
 					</li>
 				</ul>
 			</div>
