7584d3397026862fba0c0b8258ae69123aa214da
NavarroNorielJon <2161880@slu.edu.ph>
2018-06-29 11:13:21 +0800

    Update

--- src/admin/user_information/user_information.php
+++ src/admin/user_information/user_information.php
@@ -23,7 +23,7 @@
 <body>		
 	<div id="wrapper">
 		<nav class="navbar fixed-top navbar-expand-lg navbar-dark" id="navigation-bar">
-			<a href="../index" class="navbar-brand" style="margin-right:48vw;">Vivixx</a>
+			<a href="../accounts/accounts_status.php" class="navbar-brand" style="margin-right:48vw;">Vivixx</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-content" aria-controls="#navbar-content" aria-expanded="false" aria-label="Toggle navigation">
     			<span class="navbar-toggler-icon"></span>
 			</button>
