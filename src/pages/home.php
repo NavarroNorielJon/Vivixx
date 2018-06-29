8607528e23a51fd04bfd1e16e96c9340ed7aafc2
NavarroNorielJon <2161880@slu.edu.ph>
2018-06-29 16:43:13 +0800

    Update

--- src/pages/home.php
+++ src/pages/home.php
@@ -111,8 +111,8 @@
 
 			<div id="content">
 				<div class="card cards">
-						<h4 class="card-title">Upcoming Events</h4>
 						<div class="card-body">
+							<h4 class="card-title">Notification</h4>
 							<p class="card-text">
 								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
 								dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
