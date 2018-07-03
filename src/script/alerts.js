025a39e40d1d51b8b96383c16cabff4cd0412ecb
Marc Lawrence <gregoriorenz12@gmail.com>
2018-07-03 17:58:54 +0800

    update

--- src/script/alerts.js
+++ src/script/alerts.js
@@ -68,7 +68,7 @@ $('#signup_form').ajaxForm({
     }
 });
 
-$('#leave_form').ajaxForm({
+$('#leave-form').ajaxForm({
     url: '../utilities/leave_request.php',
     method: 'post',
     error: function (){
