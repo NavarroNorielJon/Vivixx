c6e42e711c70aa73985086fbb14e21ad9f558ed3
NavarroNorielJon <2161880@slu.edu.ph>
2018-07-05 16:56:22 +0800

    Update

--- src/pages/home.php
+++ src/pages/home.php
@@ -108,7 +108,7 @@
 					<hr>
 
 					<li>
-						<a href="../utilities/logout.php" class="sidebar-item" id="logout">
+						<a href="../utilities/logout.php" class="sidebar-item logout">
 							<i class="material-icons">power_settings_new</i>
 							Logout
 						</a>
@@ -120,23 +120,7 @@
 				</ul>
 			</nav>
 
-			<div id="content">
-				<div class="card cards">
-						<div class="card-body">
-							<h4 class="card-title">Notification</h4>
-							<p class="card-text">
-								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-								dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-							</p>
-
-							<div style="text-align: right">
-								<a>
-									<button type="button" class="btn btn-primary">See more</button>
-								</a>
-							</div>
-						</div>
-					</div>
-
+			<div class="content">
 				<div class="card cards">
 					<div class="card-body">
 						<h4 class="card-title">Announcements</h4>
