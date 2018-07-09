6da584fc2029908e03583c7167bee15f43112879
NavarroNorielJon <2161880@slu.edu.ph>
2018-07-09 13:25:51 +0800

    Update

--- src/pages/home.php
+++ src/pages/home.php
@@ -123,9 +123,8 @@
 			<div class="content">
 				<div class="announcements">
 					<h1 class="announcement-title">Announcements:</h1>
-					<p>
-						<div id="announce" class="carousel slide carousel-fade announcement-body" data-ride="carousel">
-							<?php
+					<div id="announce" class="carousel slide carousel-fade announcement-body" data-ride="carousel">
+						<?php
 								$query = "SELECT * FROM announcement_attachments natural join announcement where CURDATE()>=start_date and CURDATE() <= end_date and departments like('%".$department."%') or departments = 'All' group by 1;";
 								$announcement = mysqli_query($connect,$query);
 								while ($row = mysqli_fetch_array($announcement)) {
@@ -141,39 +140,39 @@
 										}
 								}
 							?>
-								<div>
-									<div class="modal fade" id="announcement" role="dialog">
-										<div class="modal-dialog">
+							<div>
+								<div class="modal fade" id="announcement" role="dialog">
+									<div class="modal-dialog">
 
-											<!-- Modal content-->
-											<div class="modal-content">
-												<div class="modal-header">
-													<button type="button" class="close" data-dismiss="modal">&times;</button>
-													<h4 class="modal-title">Modal Header</h4>
-												</div>
-												<div class="modal-body">
-													<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-												</div>
-												<div class="modal-footer">
-													<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-												</div>
+										<!-- Modal content-->
+										<div class="modal-content">
+											<div class="modal-header">
+												<h4 class="modal-title">Modal Header</h4>
 											</div>
-
+											<div class="modal-body">
+												<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+											</div>
+											<div class="modal-footer">
+												<button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+											</div>
 										</div>
+
 									</div>
 								</div>
-								<div id="announce" class="carousel slide" data-ride="carousel">
-									<div class="carousel-inner">
-										<div class="carousel-item active">
-											<img class="images" src="<?php echo $attachments[0]?>" alt="First slide">
-											<div>
-												<h2 class="subject">
-													<?php echo ucwords($subjects[0])?>
-												</h2>
-											</div>
-										</div>
+							</div>
 
-										<?php for($i = 1; $i<count($announcements); $i++){
+							<div id="announce" class="carousel slide" data-ride="carousel">
+								<div class="carousel-inner">
+									<div class="carousel-item active">
+										<img class="images" src="<?php echo $attachments[0]?>" alt="First slide">
+										<div>
+											<h2 class="subject">
+												<?php echo ucwords($subjects[0])?>
+											</h2>
+										</div>
+									</div>
+
+									<?php for($i = 1; $i<count($announcements); $i++){
 										$subjects[$i] = ucwords($subjects[$i]);
 										echo "<div class=\"carousel-item\">
 											<img class=\"images\" src=\"$attachments[$i]\" alt=\"Second slide\">
@@ -184,37 +183,37 @@
 										</div>";
 									}?>
 
-									</div>
-									<a class="carousel-control-prev" href="#announce" role="button" data-slide="prev">
+								</div>
+
+								<a class="carousel-control-prev" href="#announce" role="button" data-slide="prev">
 										<span class="carousel-control-prev-icon" aria-hidden="true"></span>
 										<span class="sr-only">Previous</span>
 									</a>
-									<a class="carousel-control-next" href="#announce" role="button" data-slide="next">
+								<a class="carousel-control-next" href="#announce" role="button" data-slide="next">
 										<span class="carousel-control-next-icon" aria-hidden="true"></span>
 										<span class="sr-only">Next</span>
 									</a>
-								</div>
-					</p>
-
+							</div>
+					</div>
 					<div class="text-right" data-toggle="modal" data-target="#announcement">
 						<button class="btn btn-primary" onclick="sample();">
 						See more
 						</button>
 					</div>
-					</div>
 				</div>
 			</div>
+		</div>
 
-			<script type="text/javascript" src="../script/jquery-3.2.1.min.js"></script>
-			<script type="text/javascript" src="../script/popper.min.js"></script>
-			<script type="text/javascript" src="../script/bootstrap/bootstrap.min.js"></script>
-			<script type="text/javascript" src="../script/ajax.js"></script>
-			<script type="text/javascript">
-				$(function() {
-					$('[data-toggle="tooltip"]').tooltip();
-				});
+		<script type="text/javascript" src="../script/jquery-3.2.1.min.js"></script>
+		<script type="text/javascript" src="../script/popper.min.js"></script>
+		<script type="text/javascript" src="../script/bootstrap/bootstrap.min.js"></script>
+		<script type="text/javascript" src="../script/ajax.js"></script>
+		<script type="text/javascript">
+			$(function() {
+				$('[data-toggle="tooltip"]').tooltip();
+			});
 
-			</script>
+		</script>
 
 	</body>
 
