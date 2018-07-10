6e6b741af2a68d6adad8a37c6d144a73be39ed9d
NavarroNorielJon <2161880@slu.edu.ph>
2018-07-10 13:10:26 +0800

    Update

--- src/pages/update_information.php
+++ src/pages/update_information.php
@@ -2,9 +2,11 @@
     include '../utilities/session.php';
     if ($birth_date != null && $birth_place != null && $contact_number != null &&
 	   $gender != null && $height != null && $weight != null && $blood_type != null && $residential_address != null && $residential_zip != null && $residential_tel_no != null && $permanent_address != null && $permanent_zip != null && $permanent_tel_no != null && $citizenship != null
-	    && $civil_status != null && $sss_no != null && $tin != null && $philhealth_no != null && $pagibig_id_no != null) {
+	    && $civil_status != null && $sss_no != null && $tin != null && $philhealth_no != null && $pagibig_id_no != null & $type = "user") {
 			header("location:/pages/home");
-	} elseif ($type = "admin") {
+	}
+
+	if ($type == "admin") {
 		header("location:/admin/accounts/accounts_status");
 	}
 ?>
