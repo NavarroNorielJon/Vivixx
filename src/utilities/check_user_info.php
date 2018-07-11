f4829c8c7201005cdf4198e74e1627aaa979bbd5
NavarroNorielJon <2161880@slu.edu.ph>
2018-07-11 13:57:19 +0800

    update

--- src/utilities/check_user_info.php
+++ src/utilities/check_user_info.php
@@ -1,7 +1,10 @@
 <?php
-	if( $birth_date == null && $birth_place == null && $contact_number == null &&
+	
+	if( ($birth_date == null && $birth_place == null && $contact_number == null &&
 	   $gender == null && $height == null && $weight == null && $blood_type == null && $residential_address == null && $residential_zip == null && $residential_tel_no == null && $permanent_address == null && $permanent_zip == null && $permanent_tel_no == null && $citizenship == null
-		&& $civil_status == null && $sss_no == null && $tin == null && $philhealth_no == null && $pagibig_id_no == null) {
-	header("location:/pages/update_information");
-	} 
+		&& $civil_status == null && $sss_no == null && $tin == null && $philhealth_no == null && $pagibig_id_no == null) && $type == "user") {
+		header("location:/pages/about.php");
+	} else {
+		echo "<script>window.location = '../admin/accounts/accounts_status';</script>";
+	}
 ?>
