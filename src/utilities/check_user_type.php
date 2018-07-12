ee185a9729da9acddb24c783e62e260d6e9d1d31
NavarroNorielJon <2161880@slu.edu.ph>
2018-07-12 16:07:01 +0800

    Update

--- src/utilities/check_user_type.php
+++ src/utilities/check_user_type.php
@@ -1,5 +1,1 @@
-<?php
-	if ($type == "admin"){
-		echo "<script>window.location = '../admin/accounts/accounts_status';</script>";
-	}
-?>
+
--- src/utilities/session.php
+++ src/utilities/session.php
@@ -4,13 +4,12 @@
     $connect = Connect();
     if(isset($_SESSION['user'])){
         $current_user = $_SESSION['user'];
-        $stmt = "SELECT * FROM mis.user WHERE username='$current_user' or email='$current_user';";
+        $stmt = "SELECT * FROM user WHERE username='$current_user' or email='$current_user';";
         $results = mysqli_query($connect, $stmt);
         $row = mysqli_fetch_array($results, MYSQLI_ASSOC);
         $username = $row["username"];
         $user_id = $row['user_id'];
 		$type = $row['type'];
-		echo $type;
     }
 
 	if(!isset($_SESSION['user'])){
