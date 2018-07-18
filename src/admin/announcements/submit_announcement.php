c4a83f8b6ff2c86d9aa5734e60044c42f6fa76d2
2161920 <2161920@slu.edu.ph>
2018-07-18 13:48:36 +0800

    update

--- src/admin/announcements/submit_announcement.php
+++ src/admin/announcements/submit_announcement.php
@@ -8,7 +8,6 @@ $connect = Connect();
     $startdate = mysqli_real_escape_string($connect,$_POST["start_date"]);
     $enddate = mysqli_real_escape_string($connect,$_POST["end_date"]);
     $body = mysqli_real_escape_string($connect,$_POST["body"]);
-    $status = mysqli_real_escape_string($connect,$_POST["status"]);
     $department = $_POST["department"];
 
     $file_names = [];
@@ -48,27 +47,25 @@ $connect = Connect();
             }
         }
         $counter = 0;
-            $sql = "INSERT into `announcement` (`subject`, `announcement`, `start_date`, `end_date`, `departments`, `status`) VALUES ('$subject', '$body', '$startdate', '$enddate', '$concat', '$status');";
+            $sql = "INSERT into `announcement` (`subject`, `announcement`, `start_date`, `end_date`, `departments`) VALUES ('$subject', '$body', '$startdate', '$enddate', '$concat');";
             $connect->query($sql);
             $get_latest_announcement = "select max(announcement_id) as id from announcement;";
             $result = $connect->query($get_latest_announcement);
             $results = $result->fetch_assoc();
             $announcement_id = $results['id'];
-
-            for($x = 0; $x< count($file_names); $x++){
-                if(!empty($file_tmp_names[$x])){
+            
+            if(!empty($file_tmp_names[$x])){
+                for($x = 0; $x< count($file_names); $x++){
                     move_uploaded_file($file_tmp_names[$x], $file_paths[$x]);
                     $temp_file = base64_encode(file_get_contents("files/".$file_names[$x]));
                     $add_attachment = "INSERT INTO announcement_attachments (`attachment_name`, `attachment`, `announcement_id`) VALUES ('$file_name','$temp_file','$announcement_id');";
                     $connect->query($add_attachment);
-
-                }else{
-                    $add_attachment = "INSERT INTO announcement_attachments (`attachment_name`, `attachment`, `announcement_id`) VALUES (NULL,NULL,'$announcement_id');";
-                    $connect->query($add_attachment);
-                    header("location: announcement.php");
                 }
-
+            }else{
+                $add_attachment = "INSERT INTO announcement_attachments (`attachment_name`, `attachment`, `announcement_id`) VALUES (NULL,NULL,'$announcement_id');";
+                $connect->query($add_attachment);
             }
+            
 
     }
 header("location: announcement.php");
