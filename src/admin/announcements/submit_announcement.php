b818d1c10d81be1e756e5e19fde1df2c575fa078
2161920 <2161920@slu.edu.ph>
2018-07-20 11:43:40 +0800

    update

--- src/admin/announcements/submit_announcement.php
+++ src/admin/announcements/submit_announcement.php
@@ -54,16 +54,18 @@ $connect = Connect();
             $results = $result->fetch_assoc();
             $announcement_id = $results['id'];
             
-            if(!empty($file_tmp_names[$x])){
-                for($x = 0; $x< count($file_names); $x++){
+            
+            for($x = 0; $x< count($file_names); $x++){
+                if(!empty($file_tmp_names[$x])){
                     move_uploaded_file($file_tmp_names[$x], $file_paths[$x]);
                     $temp_file = base64_encode(file_get_contents("files/".$file_names[$x]));
                     $add_attachment = "INSERT INTO announcement_attachments (`attachment_name`, `attachment`, `announcement_id`) VALUES ('$file_name','$temp_file','$announcement_id');";
                     $connect->query($add_attachment);
-                }
-            }else{
+                }else{
                 $add_attachment = "INSERT INTO announcement_attachments (`attachment_name`, `attachment`, `announcement_id`) VALUES (NULL,NULL,'$announcement_id');";
                 $connect->query($add_attachment);
             }
+        }
     }
-header("location: announcement.php");
+    print_r($add_attachment);
+//header("location: announcement.php");
