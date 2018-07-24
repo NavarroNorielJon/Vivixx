cd4f63a374a492c407f8b6a6de313bfd4c6d6bfe
Marc Lawrence <gregoriorenz12@gmail.com>
2018-07-24 12:43:55 +0800

    update

--- src/admin/utilities/session.php
+++ src/admin/utilities/session.php
@@ -2,12 +2,21 @@
     require_once 'db.php';
     session_start();
     $connect = Connect();
+    if(isset($_SESSION['user'])){
+        $current_user = $_SESSION['user'];
+        $stmt = "SELECT * FROM user WHERE username='$current_user' or email='$current_user';";
+        $results = mysqli_query($connect, $stmt);
+        $row = mysqli_fetch_array($results, MYSQLI_ASSOC);
+        $username = $row["username"];
+        $user_id = $row['user_id'];
+		$type = $row['type'];
+    }
 	if(!isset($_SESSION['user'])){
         header('location:/');
     }else {
 		if($type === "user"){
         	header('location:/pages/home');
-    	} 
+    	}
 	}
 
 ?>
