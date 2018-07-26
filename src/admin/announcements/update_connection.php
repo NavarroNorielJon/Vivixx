d674dcfc15bdf7238081161ddaad748b443ba3c0
2161920 <2161920@slu.edu.ph>
2018-07-26 15:20:13 +0800

    update

--- src/admin/announcements/update_connection.php
+++ src/admin/announcements/update_connection.php
@@ -1,12 +1,12 @@
 <?php
 include '../utilities/session.php';
+error_reporting(0);
 $connect = Connect();
 $announcement_id = mysqli_real_escape_string($connect,$_GET["id"]);
-    if(isset($_GET["resume"])){
+    if($_GET["connection"] === 'pause'){
         $connection = "UPDATE `announcement` SET `connection`='resume' WHERE `announcement_id`='$announcement_id'";
     }else{
-        $connection = "UPDATE `announcement` SET `connection`='pause' WHERE `announcement_id`='$announcement_id'";       
+        $connection = "UPDATE `announcement` SET `connection`='pause' WHERE `announcement_id`='$announcement_id'";     
     }
     $connect->query($connection);
-    print_r($connection);
-header("location: announcement.php");
+//header("location: announcement.php");
