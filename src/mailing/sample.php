d862776a7640245528b8483288e4defd3f9012fe
Marc Lawrence <gregoriorenz12@gmail.com>
2018-07-26 22:49:45 +0800

    update new password

--- src/mailing/sample.php
+++
@@ -1,9 +0,0 @@
-<?php
-$body = file_get_contents("style.html");
-$body .= "<div id='main_content'>";
-$body .= file_get_contents("content.html");
-$body .= "<a class='pure-button pure-button-primary' href='#" . "'>Activate my Xitle account</a>";
-$body .= "<p>If you didn't register for an account in our site, please ignore this email</p>";
-$body .= "</div>";
-
-echo $body;
